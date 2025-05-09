56c1938cad5361b5d95bc7a2032801ba22501e64
sedra <f.stutz@sedrasolutions.com>
2025-05-08 18:08:54 -0700

    updated adate to work with apple silicon arm64 without using rosetta

--- adate/build.sh
+++ adate/build.sh
@@ -25,7 +25,7 @@ fi
 declare -a FULL_CMD=()
 
 function objectCleanup() {
-  if [ -f "${BASEDIR}/${APP_NAME}.o" ] && [ -z "$SAVE_BUILD_DATA" ]; then
+  if [ -f "${BASEDIR}/${APP_NAME}*.o" ] && [ -z "$SAVE_BUILD_DATA" ]; then
     FULL_CMD=("rm" "-f" "${BASEDIR}/${APP_NAME}.o")
     # shellcheck disable=SC2068
     if out=$(${FULL_CMD[@]} 2>&1); then
@@ -47,24 +47,42 @@ if [ ! -d "bin" ]; then
   fi
 fi
 
-declare -a BOPTS=()
-BOPTS=("-Ofast -DNDEBUG")
-bcheck DEBUG && BOPTS=("-v")
+if bcheck DEBUG; then
+  export _DEBUG=1
+else
+  export _DEBUG=0
+fi
 
-if [ "$(uname -s)" == "Darwin" ]; then
-  BOPTS+=("-no-pie")
-  ARCH_FLAG="machO64"
-elif [ "$(uname -s)" == "Linux" ]; then
-  BOPTS+=("-static -static-libgcc -static-libstdc++ -no-pie")
-  ARCH_FLAG="elf64"
+if bcheck VERBOSE; then
+  export _VERBOSE=1
+else
+  export _VERBOSE=0
+fi
+
+UNAME_S=$(uname -s)
+UNAME_M=$(uname -m)
+if [ "$UNAME_S" == "Darwin" ] && [ "$UNAME_M" == "arm64" ]; then
+  COMPILE_SUCCESS_INFO="${APP_NAME}_arm64, ${APP_NAME}_x86, and ${APP_NAME}_universal binaries"
+  MOVE_SUCCESS_INFO="${APP_NAME} unverisal binary"
+  FULL_CMD=("make" "-C" "${BASEDIR}" "macos-arm64" "macos-x86_64" "universal")
+  MOVE_CMD=("mv" "${BASEDIR}/${APP_NAME}_universal" "bin/${APP_NAME}")
+elif [ "$UNAME_S" == "Darwin" ]; then
+  COMPILE_SUCCESS_INFO="${APP_NAME}_x86 binary"
+  MOVE_SUCCESS_INFO="${APP_NAME} x86 binary"
+  FULL_CMD=("make" "-C" "${BASEDIR}")
+  MOVE_CMD=("mv" "${BASEDIR}/${APP_NAME}_x86_64" "bin/${APP_NAME}")
+elif [ "${UNAME_S}" == "Linux"]; then
+  COMPILE_SUCCESS_INFO="${APP_NAME}_linux binary"
+  MOVE_SUCCESS_INFO="${APP_NAME} linux binary"
+  FULL_CMD=("make" "-C" "${BASEDIR}")
+  MOVE_CMD=("mv" "${BASEDIR}/${APP_NAME}_linux" "bin/${APP_NAME}")
 else
   mlog FATAL "Currently compilation supported on Darwin (MacOS) and Linux" 1
 fi
 
-FULL_CMD=("nasm" "-f" "$ARCH_FLAG" "$BASEDIR/$APP_NAME.asm")
 # shellcheck disable=SC2068
 if out="$(${FULL_CMD[@]} 2>&1)"; then
-  mlog SUCCESS "Successfully compiled $APP_NAME object"
+  mlog SUCCESS "Successfully compiled $COMPILE_SUCCESS_INFO"
   mlog DEBUG "Compilation Command=${FULL_CMD[*]}"
   mlog VERBOSE "Compilation Command=${FULL_CMD[*]}"
   [[ -n "$out" ]] && mlog DEBUG "$out"
@@ -72,12 +90,11 @@ else
   mlog FATAL "Failed to compile $APP_NAME\nCompilation Command=${FULL_CMD[*]}\n$out" 1
 fi
 
-FULL_CMD=("$C_CMD" "${BOPTS[@]}" -o "bin/${APP_NAME}" -Wall "${BASEDIR}/$APP_NAME".o)
 # shellcheck disable=SC2068
-if out="$(${FULL_CMD[@]} 2>&1)"; then
-  mlog SUCCESS "Successfully built $APP_NAME (binary installed at bin/$APP_NAME)"
-  mlog DEBUG "Compilation Command=${FULL_CMD[*]}"
-  mlog VERBOSE "Compilation Command=${FULL_CMD[*]}"
+if out="$(${MOVE_CMD[@]} 2>&1)"; then
+  mlog SUCCESS "Successfully built ${MOVE_SUCCESS_INFO}, and final version is bin/${APP_NAME}"
+  mlog DEBUG "Move Command=${MOVE_CMD[*]}"
+  mlog VERBOSE "Move Command=${MOVE_CMD[*]}"
   [[ -n "$out" ]] && mlog DEBUG "$out"
 
   stripit "bin/${APP_NAME}"
@@ -85,27 +102,17 @@ else
   mlog FATAL "Failed to build $APP_NAME\nCompilation Command=${FULL_CMD[*]}\n$out" 1
 fi
 
-if [ -n "$ONLY_STATIC" ] || [ "$(uname)" == "Darwin" ]; then
-  objectCleanup
-  exit 0
+if [ -z "$SAVE_BUILD_DATA" ]; then
+  FULL_CMD=("make" "-C" "${BASEDIR}" "clean")
+  # shellcheck disable=SC2068
+  if out=$(${FULL_CMD[@]} 2>&1); then
+    mlog SUCCESS "Succesfully cleaned up object & intermediate binaries files"
+    mlog DEBUG "Cleanup Command=${FULL_CMD[*]}"
+    mlog VERBOSE "Cleanup Command=${FULL_CMD[*]}"
+    [[ -n "$out" ]] && mlog DEBUG "$out"
+  else
+    mlog FATAL "Failed Cleanup\nClean Command=${FULL_CMD[*]}\n$out" 1
+  fi
 fi
-
-BOPTS=("-Ofast -s -DNDEBUG -no-pie")
-bcheck DEBUG && BOPTS=("-v -no-pie")
-
-FULL_CMD=("$C_CMD" "${BOPTS[@]}" -o "bin/${APP_NAME}-dynlink" -Wall "${BASEDIR}/$APP_NAME".o)
-# shellcheck disable=SC2068
-if out=$(${FULL_CMD[@]} 2>&1); then
-  mlog SUCCESS "Successfully built ${APP_NAME}-dynlib (binary installed at bin/${APP_NAME}-dynlink)"
-  mlog DEBUG "Compilation Command=${FULL_CMD[*]}"
-  mlog VERBOSE "Compilation Command=${FULL_CMD[*]}"
-  [[ -n "$out" ]] && mlog DEBUG "$out"
-
-  stripit "bin/${APP_NAME}-dynlink"
-else
-  mlog FATAL "Failed to build ${APP_NAME}-dynlink\nCompilation Command=${FULL_CMD[*]}\n$out" 1
-fi
-
-objectCleanup
 
 exit 0
