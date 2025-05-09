16b0a903477059afc7106e303b1979ed036b2d61
Frank Stutz <frank@franksplace.net>
2025-05-09 13:42:52 -0700

    Updated Assembly adate to work with apple silicon without rosetta (#5)
    * removed nasmVerCheck as don't have big requirement on version or vendor, and updated github action workflow to install nasm as part of prereq
    
    * fixed adate to work with macos and linux and update benchmark to work to include adate dynamic
    
    * fixed build bug where DEBUG flag did not have -no-pie in compilation
    
    * fixed adate build script to properly exit on success
    
    * added additional step for standard build and added description for the other build as debug & verbose
    
    * updated README to include assembly
    
    * updated exit codes for successful standard builds for ccdate, cdate, and godate
    
    * updated stripit to return 0 on success and 1 on error
    
    * updated adate to work with apple silicon arm64 without using rosetta
    
    * fixed but with if/elif on Linux
    
    * updated readme with apple as for silicon ARM64 macos
    
    * removed not needed objectCleanup in adate build.sh
    
    ---------
    
    Co-authored-by: sedra <f.stutz@sedrasolutions.com>

---
+++ adate/adate-Darwin-arm64.asm
@@ -0,0 +1,86 @@
+.arch armv8-a
+.section __DATA,__data
+iso_fmt:    .asciz  "%Y-%m-%dT%H:%M:%S"
+output_fmt: .asciz  "%s.%06ld%c%02ld%02ld\n"
+
+.section __DATA,__bss
+    .p2align 3
+tv:     .space 16
+tm:     .space 56
+time_str: .space 20
+
+.text
+.global _main
+.p2align 2
+
+_main:
+    stp x29, x30, [sp, #-16]!
+    mov x29, sp
+
+    adrp x0, tv@PAGE
+    add x0, x0, tv@PAGEOFF
+    mov x1, #0
+    bl _gettimeofday
+
+    adrp x0, tv@PAGE
+    add x0, x0, tv@PAGEOFF
+    adrp x1, tm@PAGE
+    add x1, x1, tm@PAGEOFF
+    bl _localtime_r
+
+    adrp x0, time_str@PAGE
+    add x0, x0, time_str@PAGEOFF
+    mov x1, #20
+    adrp x2, iso_fmt@PAGE
+    add x2, x2, iso_fmt@PAGEOFF
+    adrp x3, tm@PAGE
+    add x3, x3, tm@PAGEOFF
+    bl _strftime
+
+    adrp x8, tm@PAGE
+    add x8, x8, tm@PAGEOFF
+    ldr w9, [x8, #40]
+    mov w10, '+'
+    tbnz w9, #31, negative
+positive:
+    mov w13, #3600
+    udiv w11, w9, w13
+    mul w14, w11, w13
+    sub w12, w9, w14
+    mov w13, #60
+    udiv w12, w12, w13
+    b printf_args
+negative:
+    neg w9, w9
+    mov w10, '-'
+    b positive
+
+printf_args:
+    adrp x0, output_fmt@PAGE
+    add x0, x0, output_fmt@PAGEOFF
+    adrp x1, time_str@PAGE
+    add x1, x1, time_str@PAGEOFF
+    adrp x2, tv@PAGE
+    add x2, x2, tv@PAGEOFF
+    ldr x2, [x2, #8]
+    mov x3, x10
+    mov x4, x11
+    mov x5, x12
+
+    sub sp, sp, #48
+    str x1, [sp]
+    str x2, [sp, #8]
+    str x3, [sp, #16]
+    str x4, [sp, #24]
+    str x5, [sp, #32]
+    bl _printf
+    add sp, sp, #48
+
+    movz x16, #0x2000, lsl #16
+    movk x16, #0x0001
+    mov x0, #0
+    svc #0x80
+
+    ldp x29, x30, [sp], #16
+    ret
+
--- adate/build.sh
+++ adate/build.sh
@@ -22,23 +22,6 @@ if ! type -afp nasm >/dev/null 2>&1; then
   mlog FATAL "Assembly compiled nasm not installed or not in PATH.\nFailed to compile $APP_NAME" 1
 fi
 
-declare -a FULL_CMD=()
-
-function objectCleanup() {
-  if [ -f "${BASEDIR}/${APP_NAME}.o" ] && [ -z "$SAVE_BUILD_DATA" ]; then
-    FULL_CMD=("rm" "-f" "${BASEDIR}/${APP_NAME}.o")
-    # shellcheck disable=SC2068
-    if out=$(${FULL_CMD[@]} 2>&1); then
-      mlog SUCCESS "Succesfully deleted ${APP_NAME}.o object file"
-      mlog DEBUG "rm Command=${FULL_CMD[*]}"
-      mlog VERBOSE "rm Command=${FULL_CMD[*]}"
-      [[ -n "$out" ]] && mlog DEBUG "$out"
-    else
-      mlog FATAL "Failed remove ${APP_NAME}.o\nrm Command=${FULL_CMD[*]}\n$out" 1
-    fi
-  fi
-}
-
 if [ ! -d "bin" ]; then
   mlog INFO "Creating bin directory"
   #  out=$(mkdir bin 2>&1)
@@ -47,24 +30,45 @@ if [ ! -d "bin" ]; then
   fi
 fi
 
-declare -a BOPTS=()
-BOPTS=("-Ofast -DNDEBUG")
-bcheck DEBUG && BOPTS=("-v")
+if bcheck DEBUG; then
+  export _DEBUG=1
+else
+  export _DEBUG=0
+fi
 
-if [ "$(uname -s)" == "Darwin" ]; then
-  BOPTS+=("-no-pie")
-  ARCH_FLAG="machO64"
-elif [ "$(uname -s)" == "Linux" ]; then
-  BOPTS+=("-static -static-libgcc -static-libstdc++ -no-pie")
-  ARCH_FLAG="elf64"
+if bcheck VERBOSE; then
+  export _VERBOSE=1
+else
+  export _VERBOSE=0
+fi
+
+declare -a FULL_CMD=()
+declare UNAME_S UNAME_M
+
+UNAME_S=$(uname -s)
+UNAME_M=$(uname -m)
+if [ "$UNAME_S" == "Darwin" ] && [ "$UNAME_M" == "arm64" ]; then
+  COMPILE_SUCCESS_INFO="${APP_NAME}_arm64, ${APP_NAME}_x86, and ${APP_NAME}_universal binaries"
+  MOVE_SUCCESS_INFO="${APP_NAME} unverisal binary"
+  FULL_CMD=("make" "-C" "${BASEDIR}" "macos-arm64" "macos-x86_64" "universal")
+  MOVE_CMD=("mv" "${BASEDIR}/${APP_NAME}_universal" "bin/${APP_NAME}")
+elif [ "$UNAME_S" == "Darwin" ]; then
+  COMPILE_SUCCESS_INFO="${APP_NAME}_x86 binary"
+  MOVE_SUCCESS_INFO="${APP_NAME} x86 binary"
+  FULL_CMD=("make" "-C" "${BASEDIR}")
+  MOVE_CMD=("mv" "${BASEDIR}/${APP_NAME}_x86_64" "bin/${APP_NAME}")
+elif [ "${UNAME_S}" == "Linux" ]; then
+  COMPILE_SUCCESS_INFO="${APP_NAME}_linux binary"
+  MOVE_SUCCESS_INFO="${APP_NAME} linux binary"
+  FULL_CMD=("make" "-C" "${BASEDIR}")
+  MOVE_CMD=("mv" "${BASEDIR}/${APP_NAME}_linux" "bin/${APP_NAME}")
 else
   mlog FATAL "Currently compilation supported on Darwin (MacOS) and Linux" 1
 fi
 
-FULL_CMD=("nasm" "-f" "$ARCH_FLAG" "$BASEDIR/$APP_NAME.asm")
 # shellcheck disable=SC2068
 if out="$(${FULL_CMD[@]} 2>&1)"; then
-  mlog SUCCESS "Successfully compiled $APP_NAME object"
+  mlog SUCCESS "Successfully compiled $COMPILE_SUCCESS_INFO"
   mlog DEBUG "Compilation Command=${FULL_CMD[*]}"
   mlog VERBOSE "Compilation Command=${FULL_CMD[*]}"
   [[ -n "$out" ]] && mlog DEBUG "$out"
@@ -72,12 +76,11 @@ else
   mlog FATAL "Failed to compile $APP_NAME\nCompilation Command=${FULL_CMD[*]}\n$out" 1
 fi
 
-FULL_CMD=("$C_CMD" "${BOPTS[@]}" -o "bin/${APP_NAME}" -Wall "${BASEDIR}/$APP_NAME".o)
 # shellcheck disable=SC2068
-if out="$(${FULL_CMD[@]} 2>&1)"; then
-  mlog SUCCESS "Successfully built $APP_NAME (binary installed at bin/$APP_NAME)"
-  mlog DEBUG "Compilation Command=${FULL_CMD[*]}"
-  mlog VERBOSE "Compilation Command=${FULL_CMD[*]}"
+if out="$(${MOVE_CMD[@]} 2>&1)"; then
+  mlog SUCCESS "Successfully built ${MOVE_SUCCESS_INFO}, and final version is bin/${APP_NAME}"
+  mlog DEBUG "Move Command=${MOVE_CMD[*]}"
+  mlog VERBOSE "Move Command=${MOVE_CMD[*]}"
   [[ -n "$out" ]] && mlog DEBUG "$out"
 
   stripit "bin/${APP_NAME}"
@@ -85,27 +88,17 @@ else
   mlog FATAL "Failed to build $APP_NAME\nCompilation Command=${FULL_CMD[*]}\n$out" 1
 fi
 
-if [ -n "$ONLY_STATIC" ] || [ "$(uname)" == "Darwin" ]; then
-  objectCleanup
-  exit 0
+if [ -z "$SAVE_BUILD_DATA" ]; then
+  FULL_CMD=("make" "-C" "${BASEDIR}" "clean")
+  # shellcheck disable=SC2068
+  if out=$(${FULL_CMD[@]} 2>&1); then
+    mlog SUCCESS "Succesfully cleaned up object & intermediate binaries files"
+    mlog DEBUG "Cleanup Command=${FULL_CMD[*]}"
+    mlog VERBOSE "Cleanup Command=${FULL_CMD[*]}"
+    [[ -n "$out" ]] && mlog DEBUG "$out"
+  else
+    mlog FATAL "Failed Cleanup\nClean Command=${FULL_CMD[*]}\n$out" 1
+  fi
 fi
-
-BOPTS=("-Ofast -s -DNDEBUG -no-pie")
-bcheck DEBUG && BOPTS=("-v -no-pie")
-
-FULL_CMD=("$C_CMD" "${BOPTS[@]}" -o "bin/${APP_NAME}-dynlink" -Wall "${BASEDIR}/$APP_NAME".o)
-# shellcheck disable=SC2068
-if out=$(${FULL_CMD[@]} 2>&1); then
-  mlog SUCCESS "Successfully built ${APP_NAME}-dynlib (binary installed at bin/${APP_NAME}-dynlink)"
-  mlog DEBUG "Compilation Command=${FULL_CMD[*]}"
-  mlog VERBOSE "Compilation Command=${FULL_CMD[*]}"
-  [[ -n "$out" ]] && mlog DEBUG "$out"
-
-  stripit "bin/${APP_NAME}-dynlink"
-else
-  mlog FATAL "Failed to build ${APP_NAME}-dynlink\nCompilation Command=${FULL_CMD[*]}\n$out" 1
-fi
-
-objectCleanup
 
 exit 0
